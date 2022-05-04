82bf9eb9436ffdee6099071c17a967c2c0779bb0
Habon Assoweh <habonassoweh@Habons-Air>
2022-05-04 01:52:37 -0400

    current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,65 @@
+let weather = {
+  apiKey: "365f0c9da7f309012554f6f56b697b3c",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+
+  displayWeather: function (data) {
+    let { name } = data;
+    let { description } = data.weather[0];
+    let { temp, humidity } = data.main;
+    let { speed } = data.wind;
+
+    var cityEL = (document.querySelector(".city").innerText =
+      "Weather in " + name);
+
+    var descriptionEl = (document.querySelector(".description").innerText =
+      description);
+
+    var tempEl = (document.querySelector(".temp").innerText = temp + "°C");
+
+    var humidityEl = (document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%");
+
+    var windEl = (document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h");
+
+    var weatherEl = document
+      .querySelector(".weather")
+      .classList.remove("loading");
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Ottawa");
+
 var getFiveDay = function () {
   var city = document.querySelector(".search-bar").value;
 };
@@ -23,65 +85,3 @@ var getCityCo = function (city) {
     }
   });
 };
-
-let weather = {
-  apiKey: "365f0c9da7f309012554f6f56b697b3c",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
-      .then((data) => this.displayWeather(data));
-  },
-
-  search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
-  },
-};
-
-displayWeather = function (data) {
-  var { name } = data;
-  var { description } = data.weather[0];
-  var { temp, humidity } = data.main;
-  var { speed } = data.wind;
-
-  var cityEL = (document.querySelector(".city").innerText =
-    "Weather in " + name);
-
-  var descriptionEl = (document.querySelector(".description").innerText =
-    description);
-
-  var tempEl = (document.querySelector(".temp").innerText = temp + "°C");
-
-  var humidityEl = (document.querySelector(".humidity").innerText =
-    "Humidity: " + humidity + "%");
-
-  var windEl = (document.querySelector(".wind").innerText =
-    "Wind speed: " + speed + " km/h");
-
-  var weatherEl = document
-    .querySelector(".weather")
-    .classList.remove("loading");
-};
-
-document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
-
-weather.fetchWeather("Ottawa");
-document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
-});
